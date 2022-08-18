d5e7b8ec134d58cb7c0818706ee07069a158fc16
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-08-18 20:50:53 +0300

    Completed message

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+    return drivers.filter (
+        (matchingName) => matchingName.toLowerCase() === string.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter (
+        (matchingName) => matchingName.toLowerCase().indexOf(string.toLowerCase()) === 0
+    );
+}
+
+function matchName(drivers, string){
+    return drivers.filter((data => data.name === string));
+
+}
